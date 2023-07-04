83fc1691eb3c92b4fd8c551e232f3687e4b448cd
unknown <safaboukraa5@gmail.com>
2023-07-04 15:21:45 +0100

    fizz

---
+++ Week2/Day 2/Practice/fizz.js
@@ -0,0 +1,12 @@
+for(var i=1; i<101;i++){
+    if(i%15==0){
+        console log ("faizzbuzz");
+    }else if (i%5==0){
+        console log("buzz")
+    }else if (i%3==0){
+    console log ("fizz");
+    }else{
+        console log(i);
+    }
+
+}
